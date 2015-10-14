1edfcbb63a985b99c0f3a35d5e063f963855788c
= <cho7673@gmail.com>
2015-10-14 14:01:29 +0900

    WebView (input에 엔터키 입력시 url 이동하는것 추가해보기)

---
+++ app/src/main/java/com/hw/corcow/samplewebview/MainActivity.java
@@ -0,0 +1,108 @@
+package com.hw.corcow.samplewebview;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText urlView;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        urlView = (EditText)findViewById(R.id.input_url);
+        webView = (WebView)findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {      // ** 이 작업을 안해줄 시 브라우저를 통해 웹을 실행함.
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                    try {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                        return true;
+                    } catch (Exception e) {
+
+                    }
+                    return false;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                Log.i("MainActivity", "progress : " + newProgress);
+            }
+        });
+
+        webView.loadUrl("http://www.google.com");
+
+
+        Button btn;
+
+        btn = (Button)findViewById(R.id.btn_go);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = urlView.getText().toString();
+                if (!TextUtils.isEmpty(url)) {
+                    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                        url = "http://" + url;
+                    }
+                    webView.loadUrl(url);
+                }
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_back);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (webView.canGoBack())
+                    webView.goBack();
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_forward);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (webView.canGoForward())
+                    webView.goForward();
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        webView.pauseTimers();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        webView.resumeTimers();
+    }
+}
